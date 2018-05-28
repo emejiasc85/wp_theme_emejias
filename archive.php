efe58e7f1270dfe24a41550d1d113a7c1df960cc
emejiasc85 <emejiasc85@gmail.com>
2018-05-28 12:06:43 -0600

    refactor

--- archive.php
+++ archive.php
@@ -9,45 +9,35 @@
 
 get_header();
 ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
+			<header class="header" style="background-image: url(<?php echo( get_header_image() ); ?>);">
+				<div class="container">
+					<?php include TEMPLATEPATH ."/template-parts/nav.php"; ?>
+					<div class="jumbotron">
+						<h1 class="display-4"><?php the_archive_title(); ?></h1>
+						<?php the_archive_description(); ?> 
+						<p class="lead">
+							<a class="btn btn btn-outline-danger btn-lg btn-outline-rounded" href="<?php echo get_permalink(27) ?>">Contáctanos</a>
+						</p>
+					</div>
+				</div>
+			</header>
+			<div class="container">
+					<?php if ( have_posts() ) :
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
+							get_template_part( 'template-parts/content', get_post_type() );
+						endwhile;
+						the_posts_navigation();
+					else :
+						get_template_part( 'template-parts/content', 'none' );
+					endif;	 
+					?>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
